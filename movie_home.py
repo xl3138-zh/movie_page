409980730e9093bb3f94c794f5cf8dcea3e5a311
xl3138-zh <xl3138.zhang@gmail.com>
2023-02-28 22:44:54 +0100

    Input widgets are (almost) in place. Next step would be making some recommendataions. Will pause for a while.

--- movie_home.py
+++ movie_home.py
@@ -60,11 +60,7 @@ pages_notshown = {
 # Display the selected page
 #pages[page]()
 
-
-
-def main():
-    home_page()
-
+def left_sidebar():
     img_url = "https://storage.googleapis.com/afs-prod/media/e53811360eed4b8ba26b5f635d703a7c/3000.jpeg"
     img = Image.open(urllib.request.urlopen(img_url)) # Opens the image from the url
     with st.sidebar:
@@ -81,181 +77,75 @@ def main():
         #algorithm = st.selectbox(
         algorithm = st.radio(
             "How would you like to get movie recommendations?",
-            ("Random", "Highest rating", "Based on your taste")
+            ("Random", "Highest rating", "Based on your taste"),
+            key='k_radio_algo'
         )
         # Using "with" notation
         rate_range = st.select_slider(
             'Rating', 
             options=[i*0.5 for i in range(2,11)],
-            value = (1, 5))
- #       rate_range_c = st.slider('Rating', 1, 5, value=(1,5))
+            value = (1, 5),
+            key='k_sslider_raterange')
         movie_titles = movies.title.to_list()
-        
-        movies_select = st.multiselect("Mark movies you have seen", movie_titles)
-        col1, col2 = st.columns([3, 1])
-        movies_rated_title = []
-        movies_rated_rate = []
-        for i in range(5):
-            movie_in = col1.selectbox("Choose a movie", movie_titles)
-            rate_in = col2.select_slider('', options=[i*0.5 for i in range(2,11)], value=3)
-            movies_rated_title.append(movie_in)
-            movies_rated_rate.append(rate_in)
 
-    algorithm
-    rate_range
-#    rate_range_c
+        st.write('How flexible are you? (0: rigid, 2: fluid)')
+        flex = st.slider('flexibility', 0, 2, value=1, key='k_flex')
+
+
+        movies_select = st.multiselect(
+            "Mark movies you have seen", 
+            movie_titles, key='k_mselect_movies')
+        col1, col2 = st.columns([3, 1])
+        for i in range(5):
+            movie_in = col1.selectbox(
+                "", movie_titles, 
+                key=f'k_sbox_movie_in{i}')
+            rate_in = col2.select_slider(
+                '', options=[i*0.5 for i in range(2,11)],
+                value=3,
+                key=f'k_sslider_rate_in{i}')
+
+
+
+def main():
+    home_page()
+    left_sidebar()
+
+    movies_rated_title = []
+    movies_rated_rate = []
+    for i in range(5):
+        movies_rated_title.append(st.session_state[f'k_sbox_movie_in{i}'])
+        movies_rated_rate.append(st.session_state[f'k_sslider_rate_in{i}'])
+
+
+    algorithm = st.session_state.k_radio_algo
+    rate_range = st.session_state.k_sslider_raterange
+    movies_select = st.session_state.k_mselect_movies
+
+    st.write(f'Way to give recommendation: {algorithm}')
+    st.write(f'Recommended movies will have user rating between: {rate_range}')
+    
     movies_select
+
+    st.write('User rated movies and ratings:')
     movies_rated_title
     movies_rated_rate
 
-    
-
-    import streamlit.components.v1 as components  # Import Streamlit
-
-    # Render the h1 block, contained in a frame of size 200x200.
-    components.html("<html><body><h1>Hello, World</h1></body></html>", width=200, height=200)
 
     #background-image: url("https://yourimageurl.com");
     #background-image: "static/timothy-eberly-small.jpg";
+    #    background-image: url("timothy-eberly-small.jpg");    
 
     background_image = '''
     <style>
     body {
-        background-image: url("timothy-eberly-small.jpg");    
+        background-image: url("static/timothy-eberly-small.jpg");    
         background-size: cover;
     }
     </style>
     '''
-
     st.markdown(background_image, unsafe_allow_html=True)
-    return
-
-    st.markdown(background_image)
-
-    # your Streamlit code here...
-
-    price_range = st.slider('Select price range', 20, 100, (20, 100))
-    st.write('Price range:', price_range)
-
-    countries = [
-        "United States", "China", "Japan", "Germany", "United Kingdom", 
-        "India", "France", "Italy", "Canada", "South Korea",
-        "Russia", "Australia", "Spain", "Mexico", "Indonesia", 
-        "Netherlands", "Turkey", "Switzerland", "Saudi Arabia", "Taiwan"
-    ]
-
-    selected_country = st.select_slider('Select a country', options=countries)
-    st.write('Selected country:', selected_country)
-
-    ## Data exploration 
-    # Let's give a title
-    st.title("Binomials Awesome Pingu 🐧 EDA ")
-
-    # first section Penguins Dataset
-    st.header("Penguins data")
-    st.image("penguins.png")
-    st.write("The ```palmerpenguins``` dataset contains measurements about penguins")
-
-
-
-    # ### Task1:
-    # ### Load the 'penguins_pimped.csv' file into a data frame df
-    # ### Print out 5 random sample from df 
-    # ### (Hint: apply the function sample() on df)
-
-    df = pd.read_csv('./data/penguins_pimped.csv')
-    df_sample = df.sample(5)
-
-    # Let's show a dataframe in the app
-    st.markdown('**Hey here a 🐧 dataframe**')
-    st.dataframe(data=df_sample)
-
-    #print(df_sample)
-
-    ### Task2:
-    ### 2.1 Determine which islands are present in the data
-    island = df.island.unique()
-    #print(island)
-
-    ### 2.2 Display the data for an island you choose from the dataframe 
-
-    my_island = 'Biscoe'
-
-    my_island_df = df[df['island'] == my_island]
-
-    print(my_island_df.head())
-
-    # Adding a selectbox in combination with checkbox
-    st.markdown("**Hey here you can select stuff**")
-
-    island = st.selectbox("Select an Island", df.island.unique())
-    if st.checkbox("Do you want filter the dataframe??"):
-        st.dataframe(df[df['island'] == island])
-
-    ### Task3
-    ### Plotting
-    ### Display a scatterplot: bill_length_mm vs bill_depth
-
-
-
-    st.header("Some plotting")
-    st.subheader("Plotting with matplotlib and seaborn")
-
-    fig, ax = plt.subplots()
-    ax = sns.scatterplot(
-        data=df,
-        x='bill_length_mm',
-        y='bill_depth_mm',
-        hue='sex'
-        )
-    #plt.show()
-    #plt.close(fig)
-    st.pyplot(fig)
-
-    # Add a checkbox for showing the code
-    if st.checkbox("Do you wanna see the code 🚴🏽‍♀️?"):
-        st.markdown("""```python 
-        fig, ax = plt.subplots()
-        ax = sns.scatterplot(
-        data=df,
-        x='bill_length_mm',
-        y='bill_depth_mm',
-        hue='sex'
-        )
-        """)
-
-
-    ### Click on X in the figure-window that pop up
-
-
-
-    ### Task4
-    ### Determine the average of the bill_length_mm by species
-    ### Add Barchart
-
-    st.markdown("**Hey here descriptive statistics 😎**")
-    bill_length_mean = df.groupby(by='species')[['bill_length_mm']].mean()
-
-    #print(bill_length_mean)
-    st.bar_chart(bill_length_mean)
-
-
-    # plotly chart
-
-    fig = px.scatter(
-        data_frame=df,
-        x='bill_length_mm',
-        y='bill_depth_mm',
-        color="sex",
-        animation_frame="species"
-    )
-
-    st.plotly_chart(fig)
-
-
-    # Plotting on map 
-
-    st.map(df)
+  
 
 if __name__ == '__main__':
     main()
